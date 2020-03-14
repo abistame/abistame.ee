53d2460c2b9a58a41ab6b345ff20bdc08a3f44b2
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-03-14 09:27:19 +0200

    Add simple routing

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { RegisterForm } from './components/registerForm'
-import './App.css';
-import Map from './Map'
+import './App.css'
+import Map from './components/Map'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
 
 function App() {
-	return (
-		<div className="App">
-			<RegisterForm />
-			<Map />
-		</div>
-	);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/foobar">
+          Hello
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <RegisterForm />
+            <Map />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 
 }
 
